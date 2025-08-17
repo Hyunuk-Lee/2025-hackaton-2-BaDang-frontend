b0497d183f541c5ed8b84cb068bc29ff6100a0f6
Seonwoo Choi <seonwoo31@dgu.ac.kr>
2025-08-17 19:12:55 +0900

    Add: 메인페이지 nothing card 추가 Fix: 메인페이지 사장님 이름 & 리뷰분석 가게 이름 API 데이터로 연결 Fix: 푸터 패딩, font-weight 수정

--- src/components/MainC/BigCards.jsx
+++ src/components/MainC/BigCards.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import styled from 'styled-components';
 import NewsCard from '../NewsCard';
 import BlueCardBtn from './BigCardBtn';
+import NothingCard from './NothingCard';
+import NotebookIcon from '../../assets/Icons/NotebookIcon.svg';
+import RobotIcon from '../../assets/Icons/RobotIcon.svg';
 
 const Page=styled.div`
     display: flex;
@@ -12,12 +15,21 @@ const Page=styled.div`
     gap: 24px;
 `
 
-function BigCards() {
+function BigCards({ newsletters = [] }) {
+  // newsletters: 최신 뉴스레터 최대 2개 들어있는 배열
+  const firstCard = newsletters[0]
+    ? <NewsCard data={newsletters[0]} />
+    : <NothingCard imageUrl={NotebookIcon} text="앗! 아직 보고서가 없어요" />;
+
+  const secondCard = newsletters[1]
+    ? <NewsCard data={newsletters[1]} />
+    : <NothingCard imageUrl={RobotIcon} text="앗! 아직 보고서가 없어요" />;
+
   return (
     <Page>
-     <NewsCard/> 
-     <NewsCard/>
-      <BlueCardBtn/>
+      {firstCard}
+      {secondCard}
+      <BlueCardBtn />
     </Page>
   );
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,18 +1,16 @@
 // pages/MainPage.jsx
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Advertisement from '../components/MainC/Advertisement';
 import BigCards from '../components/MainC/BigCards';
 import SmallCards from '../components/MainC/SmallCards';
-
 const Page=styled.div`
 display: flex;
 width: 1200px;
 flex-direction: column;
 height: 1238px;
 min-height: 100vh;
-
 align-items: flex-start;
 `
 const HelloText=styled.div`
@@ -43,9 +41,28 @@ const CardWrapper=styled.div`
 
 
 function MainPage() {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    // 예시: 로그인 후 현재 사용자 정보 가져오기
+    const fetchUserName = async () => {
+      try {
+        const response = await fetch('/api/users/current'); // 현재 로그인한 사용자 정보 엔드포인트
+        if (!response.ok) throw new Error('사용자 정보 가져오기 실패');
+        const data = await response.json();
+        setName(data.name); // API에서 받은 name 사용
+      } catch (err) {
+        console.error(err);
+        setName('이름없음'); // 기본값
+      }
+    };
+
+    fetchUserName();
+  }, []);
+
   return (
     <Page>
-      <HelloText>안녕하세요 김바당 <Blue>사장님</Blue> <br/>리뷰를 <Orange>분석</Orange>하고 이번주 <Orange>보고서</Orange>를 확인해보세요!</HelloText>
+      <HelloText>안녕하세요 {name} <Blue>사장님</Blue> <br/>리뷰를 <Orange>분석</Orange>하고 이번주 <Orange>보고서</Orange>를 확인해보세요!</HelloText>
     <CardWrapper>
       <BigCards/>
       <Advertisement/>
