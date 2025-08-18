aaff0de23abdcb0abce09b9148f312c063a42367
Hyunuk-Lee <air244302@gmail.com>
2025-08-18 13:19:50 +0900

    Fix: 스크롤 문제 수정

--- src/components/BaseC/ScrollToTop.jsx
+++ src/components/BaseC/ScrollToTop.jsx
@@ -6,6 +6,10 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
+  console.log('[ScrollToTop] run', location.pathname, window.scrollY);
+}, [location.pathname, location.search, location.hash]);
+
+  useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]); // pathname이 바뀔 때마다 실행
 
