6ee5e6eb7a085b0cc9329c81a983778f6f30a7a9
Seonwoo Choi <seonwoo31@dgu.ac.kr>
2025-08-24 18:39:18 +0900

    풀을 위한 커밋

--- src/components/CoworkC/CoworkPopup.jsx
+++ src/components/CoworkC/CoworkPopup.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 import styled from 'styled-components';
 import Btn from '../CoworkC/PopupButton.jsx';
 import Close from '../../assets/Icons/XIcon.svg'; // 엑스 아이콘
-import axios from 'axios';
-
 
 const Overlay = styled.div`
   position: fixed; /* 화면 전체 덮기 */
@@ -18,99 +16,94 @@ const Overlay = styled.div`
   align-items: center;
   z-index: 999; /* 다른 요소 위에 표시 */
 `;
-const PopupBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 623px;
-  height: 357px;
-  padding: 24px 16px;
-  justify-content: center;
-  align-items: center;
-  flex-shrink: 0;
-  border-radius: 20px;
-  background: #fff;
-  gap: 48px;
+const PopupBox=styled.div`
+    display: flex;
+    flex-direction: column;
+width: 623px;
+height: 357px;
+padding: 24px 16px;
+justify-content: center;
+align-items: center;
+flex-shrink: 0;
+border-radius: 20px;
+background: #FFF;
+gap: 48px;
   position: relative;
-`;
-const TextWrapper = styled.div`
-  display: flex;
-  width: 467px;
-  flex-direction: column;
-  align-items: center;
-  gap: 48px;
-`;
-const Title = styled.div`
-  color: #17171b;
-  text-align: center;
 
-  font-family: SUIT;
-  font-size: 25px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-`;
-const CloseBtn = styled.img`
-  position: absolute;
+`
+const TextWrapper=styled.div`
+    display: flex;
+    width: 467px;
+    flex-direction: column;
+    align-items: center;
+    gap: 48px;
+`
+const Title=styled.div`
+    color: #17171B;
+    text-align: center;
+
+    font-family: SUIT;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`
+const CloseBtn=styled.img`
+    position: absolute;
   top: 16px;
   right: 16px;
   width: 24px;
   height: 24px;
   cursor: pointer;
-`;
-const ContentWrapper = styled.div`
-  gap: 8px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`;
+`
+const ContentWrapper=styled.div`
+    gap: 8px;
+    width:100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;`
 
-const Content = styled.div`
-  color: #494954;
-  text-align: center;
-  font-family: SUIT;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 28px;
-`;
-const BtnWrapper = styled.div`
-  width: 591px;
-  height: 59px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  gap: 24px;
-  align-items: center;
-`;
+const Content=styled.div`
+    color:#494954;
+    text-align: center;
+    font-family: SUIT;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 28px; 
 
-function CoworkPopup({
-  storeName,
-  storeType,
-  requestContent,
-  collaborateId,
-  onClose,
-}) {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+`
+const BtnWrapper=styled.div`
+    width: 591px;
+    height: 59px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 24px;
+    align-items: center;
+`
+
+function CoworkPopup({ storeName, storeType, requestContent, collaborateId, onClose }) {
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const handleDecision = async (isAccepted) => {
     try {
-      console.log("가게이름", storeName);
-
-      const response = await fetch(`${backendUrl}/collaboration/accept`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({
-          collaborateId: collaborateId,
-          isAccepted: isAccepted,
-        }),
-      });
+console.log("가게이름",storeName)
+      
+const response = await fetch(`${backendUrl}/collaboration/accept`, {
+  method: 'PATCH',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  credentials: 'include', 
+  body: JSON.stringify({
+    collaborateId: collaborateId,
+    isAccepted: isAccepted,
+  }),
+});
 
       if (!response.ok) {
-        console.error("서버 오류:", response.status);
+        console.error('서버 오류:', response.status);
         return;
       }
 
@@ -119,32 +112,8 @@ function CoworkPopup({
 
       onClose(); // 팝업 닫기
     } catch (err) {
-      console.error("요청 실패:", err);
+      console.error('요청 실패:', err);
     }
-  };
-
-    const handleConfirm = async () => {
-    try {
-      await axios.post(`${backendUrl}/collaboration/confirm`, { storeId: storeId }, { withCredentials: true });
-      await fetchData();
-      handleClosePopup();
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const handleDelete = async () => {
-    try {
-      await axios.delete(`${backendUrl}/collaboration/request/${storeId}`, {
-        withCredentials: true,
-      });
-      // 부모 컴포넌트에 삭제 사실 전달
-      onDelete(storeId);
-      handleClosePopup(); // 팝업 닫기
-  } catch (err) {
-    console.error("삭제 실패:", err);
-    alert("삭제에 실패했습니다.");
-  }
   };
 
   return (
@@ -156,17 +125,15 @@ function CoworkPopup({
             {storeName}의 <br /> 협업 요청을 수락할까요?
           </Title>
           <ContentWrapper>
-            <Content>
-              {storeName} : {storeType}
-            </Content>
+            <Content>{storeName} : {storeType}</Content>
             <Content>요청 내용 : {requestContent}</Content>
           </ContentWrapper>
         </TextWrapper>
         <BtnWrapper>
           <Btn btnName='수락하기' color='#759AFC' width="221px" 
-               onClick={() => handleConfirm()} />
+               onClick={() => handleDecision('ACCEPTED')} />
           <Btn btnName='거절하기' color='#FF9762' width="221px" 
-               onClick={() => handleDelete()} />
+               onClick={() => handleDecision('REJECTED')} />
         </BtnWrapper>
       </PopupBox>
     </Overlay>
